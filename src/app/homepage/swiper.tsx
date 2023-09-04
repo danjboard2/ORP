e1069441b177ee1ad75fcfbbc56715307bccc0ab
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-09-04 16:40:26 +0100

    homepage slider styling

--- src/app/homepage/swiper.tsx
+++ src/app/homepage/swiper.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import { useState, useEffect } from 'react';
 import { Swiper as OriginalSwiper } from 'swiper';
 import { Navigation, Pagination, Scrollbar, A11y, EffectFade, Autoplay } from 'swiper/modules';
@@ -29,7 +30,6 @@ export default function HomepageSwiper() {
             if (this.isBeginning && this.loop) {
               correctedIndex = 0;
             }
-      console.log('corrected index is: ' + correctedIndex, 'number of slides is' + numberOfSlides)
             setActiveIndex(correctedIndex);
           });
         }
@@ -62,11 +62,36 @@ export default function HomepageSwiper() {
       <SwiperSlide className="!flex flex-col bg-[url('/media/images/soil.jpg')] bg-cover justify-center"><div className="flex flex-col ml-60"><h1 className="text-7xl font-bold uppercase text-white [text-shadow:_0_5px_6px_rgb(0_0_0_/_80%)]">Sewage Message Title</h1><h2 className="text-4xl my-6 font-bold text-white [text-shadow:_0_5px_6px_rgb(0_0_0_/_80%)]">Message subtitle/body text goes here</h2><Link href="/sewage">Learn more</Link></div></SwiperSlide>
       <SwiperSlide className="!flex flex-col bg-[url('/media/images/soil.jpg')] bg-cover justify-center"><div className="flex flex-col ml-60"><h1 className="text-7xl font-bold uppercase text-white [text-shadow:_0_5px_6px_rgb(0_0_0_/_80%)]">Agriculture Message Title</h1><h2 className="text-4xl my-6 font-bold text-white [text-shadow:_0_5px_6px_rgb(0_0_0_/_80%)]">Message subtitle/body text goes here</h2><Link href="/agriculture">Learn more</Link></div></SwiperSlide>
     </Swiper>
-    <div className="navigate flex flex-row justify-around">
-        <div className={`item w-[20%] ${activeIndex === 0 ? 'active' : ''}`} onClick={() => goToSlide(0)}><h3>Soil</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p><Link href="/">Learn more</Link></div>
-        <div className={`item w-[20%] ${activeIndex === 1 ? 'active' : ''}`} onClick={() => goToSlide(1)}><h3>Mold</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p><Link href="/">Learn more</Link></div>
-        <div className={`item w-[20%] ${activeIndex === 2 ? 'active' : ''}`} onClick={() => goToSlide(2)}><h3>Sewage</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p><Link href="/">Learn more</Link></div>
-        <div className={`item w-[20%] ${activeIndex === 3 ? 'active' : ''}`} onClick={() => goToSlide(3)}><h3>Agriculture</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p><Link href="/">Learn more</Link></div>
+    <div className="navigate flex flex-row justify-around mx-20">
+        <div className={`item bg-white z-10 relative text-center [box-shadow:_0_4px_4px_rgb(0_0_0_/_25%)] p-10 rounded-lg rounded-t-none w-[20%] ${activeIndex === 0 ? 'active' : ''}`} onClick={() => goToSlide(0)}>
+          <div className="h-[120px] mb-14">
+            <Image src={activeIndex === 0 ? '/media/images/soil-icon-active.png' : '/media/images/soil-icon.png'} width={80} height={80} alt="Soil" className="round-image block m-auto -mt-[120px] p-[30px] w-[150px] bg-white rounded-full [box-shadow:_0_4px_4px_rgb(0_0_0_/_25%)]"/>
+            </div>
+            <h3 className="text-3xl uppercase font-bold mb-4">Soil</h3>
+            <p className="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+            <Link href="/" className="block w-4/5 m-auto border-solid border-2 color-[#404041] rounded-full text-2xl px-10 py-2 border-[#404041] mt-6">Learn more</Link>
+        </div>
+        <div className={`item bg-white z-10 relative text-center [box-shadow:_0_4px_4px_rgb(0_0_0_/_25%)] p-10 rounded-lg rounded-t-none w-[20%]  ${activeIndex === 1 ? 'active' : ''}`} onClick={() => goToSlide(1)}>
+        <div className="h-[120px] mb-14">
+            <Image src={activeIndex === 1 ? '/media/images/soil-icon-active.png' : '/media/images/soil-icon.png'} width={80} height={80} alt="Soil" className="round-image block m-auto -mt-[120px] p-[30px] w-[150px] bg-white rounded-full [box-shadow:_0_4px_4px_rgb(0_0_0_/_25%)]"/>
+            </div>
+          <h3 className="text-3xl uppercase font-bold mb-4">Mold</h3><p className="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+          <Link href="/" className="block w-4/5 m-auto border-solid border-2 color-[#404041] rounded-full text-2xl px-10 py-2 border-[#404041] mt-6">Learn more</Link>
+          </div>
+        <div className={`item bg-white z-10 relative text-center [box-shadow:_0_4px_4px_rgb(0_0_0_/_25%)] p-10 rounded-lg rounded-t-none w-[20%]  ${activeIndex === 2 ? 'active' : ''}`} onClick={() => goToSlide(2)}>
+        <div className="h-[120px] mb-14">
+            <Image src={activeIndex === 2 ? '/media/images/soil-icon-active.png' : '/media/images/soil-icon.png'} width={80} height={80} alt="Soil" className="round-image block m-auto -mt-[120px] p-[30px] w-[150px] bg-white rounded-full [box-shadow:_0_4px_4px_rgb(0_0_0_/_25%)]"/>
+            </div>
+          <h3 className="text-3xl uppercase font-bold mb-4">Sewage</h3><p className="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+          <Link href="/" className="block w-4/5 m-auto border-solid border-2 color-[#404041] rounded-full text-2xl px-10 py-2 border-[#404041] mt-6">Learn more</Link>
+          </div>
+        <div className={`item bg-white z-10 relative text-center [box-shadow:_0_4px_4px_rgb(0_0_0_/_25%)] p-10 rounded-lg rounded-t-none w-[20%]  ${activeIndex === 3 ? 'active' : ''}`} onClick={() => goToSlide(3)}>
+        <div className="h-[120px] mb-14">
+            <Image src={activeIndex === 3 ? '/media/images/soil-icon-active.png' : '/media/images/soil-icon.png'} width={80} height={80} alt="Soil" className="round-image block m-auto -mt-[120px] p-[30px] w-[150px] bg-white rounded-full [box-shadow:_0_4px_4px_rgb(0_0_0_/_25%)]"/>
+            </div>
+          <h3 className="text-3xl uppercase font-bold mb-4">Agriculture</h3><p className="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+          <Link href="/" className="block w-4/5 m-auto border-solid border-2 color-[#404041] rounded-full text-2xl px-10 py-2 border-[#404041] mt-6">Learn more</Link>
+          </div>
     </div>
     </>
   );
