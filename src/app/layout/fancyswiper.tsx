264e1a3e97be97006d279e31df9fe4318cb5398e
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-09-09 18:27:58 +0100

    mobile fixes

--- src/app/layout/fancyswiper.tsx
+++ src/app/layout/fancyswiper.tsx
@@ -73,12 +73,12 @@ const FancySwiper: React.FC<PageContentProps & totalSlides> = ({ pages, numberSl
     <div className="w-full relative overflow-y-clip"> { /* sliding text wrapper */}
     {pages.map((page: Pages, pageIndex:number) => (
       <div key={pageIndex}>
-    <div className={`curvedbg absolute w-[2400px] h-[1300px] top-0 right-0 bottom-0 -left-[1000px] ${page.name}`} style={{
+    <div className={`curvedbg absolute w-[2400px] h-[1300px] top-0 right-0 bottom-0 -left-[1520px] sm:-left-[1420px] lg:-left-[1220px] xl:-left-[1000px] ${page.name}`} style={{
         background : page.bg,
       }}></div>
     <Swiper
     onSwiper={(swiper: OriginalSwiper) => setSwiperInstance(swiper)}
-    className={`w-[600px] !ml-[5%] min-h-[965px] h-[100vh] max-h-[1200px]  ${page.name}`}
+    className={`w-[260px] sm:w-[300px] lg:w-[600px] !ml-[5%] min-h-[965px] h-[100vh] max-h-[1200px]  ${page.name}`}
       modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade, Autoplay]}
       spaceBetween={0}
       slidesPerView={1}
@@ -96,24 +96,24 @@ const FancySwiper: React.FC<PageContentProps & totalSlides> = ({ pages, numberSl
     >     
        {page.sections.map((section: Section, sectionIndex: number) => (
         <div key={sectionIndex}>
-          <SwiperSlide className="swiper-slide-first"><div className="flex flex-col ml-[10%] w-[600px] min-h-[900px] max-h-[1000px] h-full justify-center"><div className="text-2xl my-6  pr-32" style={{
+          <SwiperSlide className="swiper-slide-first"><div className="flex flex-col xl:ml-[10%] w-[260px] sm:w-[300px] lg:w-[600px] min-h-[900px] max-h-[1000px] h-full justify-center"><div className="pb-20 sm:pb-0 text-lg sm:text-xl xl:text-2xl my-6  lg:pr-32" style={{
         color: page.color,
       }} dangerouslySetInnerHTML={{ __html: section.content }}></div></div></SwiperSlide>
           </div>
           ))}
-                <div className="block absolute top-[250px] left-[10%]">
-      <h1 className="text-5xl text-[#73CD4A] font-bold uppercase" style={{
+                <div className="cust-sw block absolute top-[250px] xl:left-[10%]">
+      <h1 className="text-2xl sm:text-4xl xl:text-5xl text-[#73CD4A] font-bold uppercase" style={{
         color: page.color,
       }}>{page.pagetitle}</h1>
       </div>
-        <div className={`custom-navigation z-10 flex flex-row w-[180px] justify-between ${page.name} ${numberOfSlides == 1 ? 'hidden' : ''}`} >
+        <div className={`custom-navigation z-10 flex flex-row w-[120px] sm:w-[180px] justify-between ${page.name} ${numberOfSlides == 1 ? 'hidden' : ''}`} >
           <div className="swiper-button-prev"><FaCircleChevronLeft size={50} className="text-[#D9D9D9]"/></div>
           <div className="swiper-button-next"><FaCircleChevronRight size={50} className="text-[#D9D9D9]"/></div>
       </div>
     </Swiper>
-    <Image src={`${page.smallimage1}`} priority={true} width={191} height={191} alt="Organic Remediation Products" className={`smallimage1 absolute top-[170px] left-[870px] ${page.name}`}/>
-    <Image src={`${page.smallimage2}`} priority={true} width={218} height={218} alt="Organic Remediation Products" className={`smallimage2 absolute top-[408px] left-[820px] ${page.name}`}/>
-    <Image src={`${page.smallimage3}`} priority={true} width={304} height={304} alt="Organic Remediation Products" className={`smallimage3 absolute top-[665px] left-[660px] ${page.name}`}/>
+    <Image src={`${page.smallimage1}`} priority={true} width={191} height={191} alt="Organic Remediation Products" className={`smallimage1 absolute top-[170px] left-[350px] sm:left-[445px] lg:left-[645px] xl:left-[870px] ${page.name}`}/>
+    <Image src={`${page.smallimage2}`} priority={true} width={218} height={218} alt="Organic Remediation Products" className={`smallimage2 absolute top-[408px] left-[305px] sm:left-[400px] lg:left-[600px] xl:left-[820px] ${page.name}`}/>
+    <Image src={`${page.smallimage3}`} priority={true} width={304} height={304} alt="Organic Remediation Products" className={`smallimage3 absolute top-[665px] left-[150px] sm:left-[240px] lg:left-[440px] xl:left-[660px] ${page.name}`}/>
     <div className="absolute top-0 left-0 right-0 bottom-0 h-[925px] -z-[1]">  { /* background images and small bubble images */}
 
         <div className={`item absolute top-0 left-0 right-0 bottom-0 h-[100vh] min-h-[965px] max-h-[1200px] bg-cover bg-right ${activeIndex === 0 ? 'active' : ''}`} onClick={() => goToSlide(0)} style={{
