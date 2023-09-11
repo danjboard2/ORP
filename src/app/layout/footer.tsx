336db1999c64a9bc39860cec62125a50b3c86573
danjboard2 <daveee@hotmail.com>
2023-09-11 21:52:40 +0100

    fixes for mobile footer

--- src/app/layout/footer.tsx
+++ src/app/layout/footer.tsx
@@ -6,15 +6,15 @@ import { SiDiscord, SiTwitter } from "react-icons/si";
 export default function Footer() {
     return (
        <footer className="flex flex-row sm:flex-col space-around bg-black w-full text-white px-10 py-8">
-         <div className="w-full flex max-w-[2000px] m-auto">
-         <div className="flex min-w-[130px] w-1/2 sm:w-1/4 items-center justify-center border-r-[1px] border-[#404041]">
+         <div className="w-full flex flex-col  sm:flex-row max-w-[2000px] m-auto">
+         <div className="flex min-w-[130px] w-full sm:w-1/2 sm:w-1/4 items-center justify-center sm:border-r-[1px] border-[#404041]">
             <Image src="/media/images/orp-logo.png" width={200} height={100} alt="ORP Logo"className="mr-8 sm:mr-4 min-w-[120px]" />
          </div>
-         <div className="flex flex-col sm:flex-row w-3/4">
+         <div className="flex flex-col sm:flex-row w-full sm:w-3/4">
 
             { /* col of footer */ }
-            <div className="flex w-1/2 sm:w-1/3 justify-center">
-               <div className="flex flex-col w-full pl-6 sm:pl-10">
+            <div className="flex w-full sm:w-1/3 justify-center">
+               <div className="flex flex-col w-full pl-6 sm:pl-10 text-center sm:text-left">
                <h4 className=" text-xl mt-6 sm:mt-0 mb-2 sm:mb-8 font-bold">Products</h4>
                <Link href="/soil" className="mb-4 hover:underline">Soil</Link>
                <Link href="/mold" className="mb-4 hover:underline">Mold</Link>
@@ -24,8 +24,8 @@ export default function Footer() {
             { /* end col of footer */}
 
             { /* col of footer */ }
-            <div className="flex w-1/2 sm:w-1/3 justify-center">
-               <div className="flex flex-col w-full pl-6  sm:pl-10">
+            <div className="flex w-full sm:w-1/3 justify-center">
+               <div className="flex flex-col w-full pl-6  sm:pl-10 text-center sm:text-left">
                <h4 className=" text-xl mt-6 sm:mt-0 mb-2 sm:mb-8 font-bold">General</h4>
                <Link href="/projects" className="mb-4 hover:underline">Projects</Link>
                <Link href="/downloads" className="mb-4 hover:underline">Documents</Link>
@@ -35,11 +35,11 @@ export default function Footer() {
             { /* end col of footer */}
       
             { /* col of footer */ }
-            <div className="flex w-1/2 sm:w-1/3 justify-center">
-               <div className="flex flex-col w-full pl-6 sm:pl-10">
+            <div className="flex w-full sm:w-1/3 justify-center">
+               <div className="flex flex-col w-full pl-6 sm:pl-10 text-center sm:text-left">
                <h4 className=" text-xl mt-6 sm:mt-0 mb-2 sm:mb-8 font-bold">Social</h4>
-               <Link href="https://twitter.com" className="mb-4"><SiTwitter size={25}/></Link>
-               <Link href="https://discord.com" className="mb-4"><SiDiscord size={25}/></Link>
+               <Link href="https://twitter.com" className="m-auto mb-4 sm:ml-0"><SiTwitter size={25}/></Link>
+               <Link href="https://discord.com" className="m-auto mb-4 sm:ml-0"><SiDiscord size={25}/></Link>
                </div>
             </div>
             { /* end col of footer */}
