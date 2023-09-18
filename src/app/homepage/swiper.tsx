2aaafa92f9c186de246eda0e926e318515b79047
danjboard2 <101198631+danjboard2@users.noreply.github.com>
2023-09-18 23:42:36 +0100

    hero

--- src/app/homepage/swiper.tsx
+++ src/app/homepage/swiper.tsx
@@ -3,7 +3,8 @@ import Image from 'next/image'
 import { useState, useEffect } from 'react';
 import { Swiper as OriginalSwiper } from 'swiper';
 import { Navigation, Pagination, Scrollbar, A11y, EffectFade, Autoplay } from 'swiper/modules';
-import { useSwiper, Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {FaCircleChevronLeft, FaCircleChevronRight} from 'react-icons/fa6'
 
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -37,14 +38,29 @@ export default function HomepageSwiper() {
       if (swiperInstance) {
         swiperInstance.slideTo(index);
       }
-    };
+    }
+      const handlePrev = () => {
+        if (swiperInstance) {
+          const currentIndex = swiperInstance.activeIndex;
+          const newIndex = currentIndex === 0 ? numberOfSlides - 1 : currentIndex - 1;
+          swiperInstance.slideTo(newIndex);
+        }
+      };
+    
+      const handleNext = () => {
+        if (swiperInstance) {
+          const currentIndex = swiperInstance.activeIndex;
+          const newIndex = currentIndex === numberOfSlides - 1 ? 0 : currentIndex + 1;
+          swiperInstance.slideTo(newIndex);
+        }
+      };
   
 
   return (
     <>
     <Swiper
     onSwiper={(swiper: OriginalSwiper) => setSwiperInstance(swiper)}
-    className="w-full h-[925px]"
+    className="w-full h-[500px] md:h-[925px]"
     id="homeswiper"
       modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade, Autoplay]}
       effect="fade"
@@ -55,24 +71,25 @@ export default function HomepageSwiper() {
         disableOnInteraction: false,
       }}
       pagination={{ clickable: true }}
-      navigation={true}
       rewind={true}
       allowTouchMove={false}
       data-swiper-autoplay={5000} 
     >
-      <SwiperSlide className="!flex flex-col justify-center px-4 lg:px-32"><Image src="/media/images/soil.jpg" id="swiperimage" className="absolute object-cover block -z-[1] top-0 left-0 right-0 bottom-0 min-w-[100%] min-h-[965px] w-auto h-auto" width={3000} height={3000} alt="Soil"></Image><div className="flex flex-col w-full max-w-[2000px] text-center lg:text-left m-auto 3xl:px-32 pt-[100px]"><h1 className="text-4xl xl:text-7xl font-bold uppercase text-white [text-shadow:_0_5px_6px_rgb(0_0_0_/_80%)]">Hydrocarbon</h1><h2 className="text-2xl xl:text-4xl my-6 font-bold text-white [text-shadow:_0_5px_6px_rgb(0_0_0_/_80%)]">ORP-SR1 & ORP-SR2</h2><Link href="/soil" className="inline-block m-auto lg:ml-0 w-fit text-lg xl:text-2xl rounded-full border-4 border-[#3AAA01] px-10 xl:px-20 py-2 font-bold text-white lg:text-[#3AAA01] mt-24 bg-[#3AAA01] lg:bg-transparent hover:bg-[#3AAA01] hover:text-white">Learn more</Link></div></SwiperSlide>
-      <SwiperSlide className="!flex flex-col justify-center px-4 lg:px-32"><Image src="/media/images/mold.jpg" id="swiperimage" className="absolute object-cover block -z-[1] top-0 left-0 right-0 bottom-0 min-w-[100%] min-h-[965px] w-auto h-auto" width={3000} height={3000} alt="Soil"></Image><div className="flex flex-col w-full max-w-[2000px] text-center lg:text-left m-auto 3xl:px-32 pt-[100px]"><h1 className="text-4xl xl:text-7xl font-bold uppercase text-white [text-shadow:_0_5px_6px_rgb(0_0_0_/_80%)]">Mold</h1><h2 className="text-2xl xl:text-4xl my-6 font-bold text-white [text-shadow:_0_5px_6px_rgb(0_0_0_/_80%)]">ORP-MT1 & ORP-MT2</h2><Link href="/mold" className="inline-block m-auto lg:ml-0 w-fit text-lg xl:text-2xl rounded-full border-4 border-[#3AAA01] px-10 xl:px-20 py-2 font-bold  text-white lg:text-[#3AAA01] mt-24 bg-[#3AAA01] lg:bg-transparent hover:bg-[#3AAA01] hover:text-white">Learn more</Link></div></SwiperSlide>
-      <SwiperSlide className="!flex flex-col justify-center px-4 lg:px-32"><Image src="/media/images/sewage.jpg" id="swiperimage" className="absolute object-cover block -z-[1] top-0 left-0 right-0 bottom-0 min-w-[100%] min-h-[965px] w-auto h-auto" width={3000} height={3000} alt="Soil"></Image><div className="flex flex-col w-full max-w-[2000px] text-center lg:text-left m-auto 3xl:px-32 pt-[100px]"><h1 className="text-4xl xl:text-7xl font-bold uppercase text-white [text-shadow:_0_5px_6px_rgb(0_0_0_/_80%)]">Sewage</h1><h2 className="text-2xl xl:text-4xl my-6 font-bold text-white [text-shadow:_0_5px_6px_rgb(0_0_0_/_80%)]">ORP-WS1 & ORP-WS2</h2><Link href="/sewage" className="inline-block  m-auto lg:ml-0 w-fit text-lg xl:text-2xl rounded-full border-4 border-[#3AAA01] px-10 xl:px-20 py-2 font-bold text-white lg:text-[#3AAA01] mt-24 bg-[#3AAA01] lg:bg-transparent hover:bg-[#3AAA01] hover:text-white">Learn more</Link></div></SwiperSlide>
+      <SwiperSlide className="!flex flex-col justify-center px-4 lg:px-32"><Image src="/media/images/soil.jpg" id="swiperimage" className="absolute object-cover object-left block -z-[1] top-0 left-0 right-0 bottom-0 min-w-[100%] min-h-[505px] md:min-h-[965px] w-auto h-auto" width={3000} height={3000} alt="Soil"></Image><div className="flex flex-col w-full max-w-[2000px] md:text-center lg:text-left m-auto 3xl:px-32 pt-[20px] md:pt-[100px]"><h1 className="text-4xl xl:text-7xl font-bold uppercase text-white [text-shadow:_0_5px_6px_rgb(0_0_0_/_80%)]">Hydrocarbon</h1><h2 className="text-2xl xl:text-4xl my-2 md:my-6 font-bold text-white [text-shadow:_0_5px_6px_rgb(0_0_0_/_80%)]">ORP-SR1 & ORP-SR2</h2><Link href="/soil" className="inline-block md:m-auto mt-2 lg:ml-0 w-fit text-lg xl:text-2xl rounded-full border-[1px] md:border-4 border-[#3AAA01] px-10 xl:px-20 md:py-2 font-bold text-[#3AAA01] md:mt-24 bg-[#3AAA01] bg-transparent hover:bg-[#3AAA01] hover:text-white">Learn more...</Link></div></SwiperSlide>
+      <SwiperSlide className="!flex flex-col justify-center px-4 lg:px-32"><Image src="/media/images/mold.jpg" id="swiperimage" className="absolute object-cover object-left block -z-[1] top-0 left-0 right-0 bottom-0 min-w-[100%]  min-h-[505px] md:min-h-[965px] w-auto h-auto" width={3000} height={3000} alt="Soil"></Image><div className="flex flex-col w-full max-w-[2000px] md:text-center lg:text-left m-auto 3xl:px-32 pt-[20px] md:pt-[100px]"><h1 className="text-4xl xl:text-7xl font-bold uppercase text-white [text-shadow:_0_5px_6px_rgb(0_0_0_/_80%)]">Mold</h1><h2 className="text-2xl xl:text-4xl my-2 md:my-6 font-bold text-white [text-shadow:_0_5px_6px_rgb(0_0_0_/_80%)]">ORP-MT1 & ORP-MT2</h2><Link href="/mold" className="inline-block md:m-auto mt-2 lg:ml-0 w-fit text-lg xl:text-2xl rounded-full border-[1px] md:border-4 border-[#3AAA01] px-10 xl:px-20 md:py-2 font-bold  text-[#3AAA01] md:mt-24 bg-[#3AAA01] bg-transparent hover:bg-[#3AAA01] hover:text-white">Learn more...</Link></div></SwiperSlide>
+      <SwiperSlide className="!flex flex-col justify-center px-4 lg:px-32"><Image src="/media/images/sewage.jpg" id="swiperimage" className="absolute object-cover object-left block -z-[1] top-0 left-0 right-0 bottom-0 min-w-[100%] min-h-[505px]  md:min-h-[965px] w-auto h-auto" width={3000} height={3000} alt="Soil"></Image><div className="flex flex-col w-full max-w-[2000px] md:text-center lg:text-left m-auto 3xl:px-32 pt-[20px] md:pt-[100px]"><h1 className="text-4xl xl:text-7xl font-bold uppercase text-white [text-shadow:_0_5px_6px_rgb(0_0_0_/_80%)]">Sewage</h1><h2 className="text-2xl xl:text-4xl my-2 md:my-6 font-bold text-white [text-shadow:_0_5px_6px_rgb(0_0_0_/_80%)]">ORP-WS1 & ORP-WS2</h2><Link href="/sewage" className="inline-block  md:m-auto mt-2  lg:ml-0 w-fit text-lg xl:text-2xl rounded-full border-[1px] md:border-4 border-[#3AAA01] px-10 xl:px-20 md:py-2 font-bold text-[#3AAA01] md:mt-24 bg-[#3AAA01] bg-transparent hover:bg-[#3AAA01] hover:text-white">Learn more...</Link></div></SwiperSlide>
     </Swiper>
-    <div className="navigate w-full max-w-[2000px] flex flex-col lg:flex-row justify-between m-auto px-4 xl:px-10 2xl:px-32 !mb-12">
+    <div className="navigate w-full relative max-w-[2000px] flex flex-col lg:flex-row justify-between m-auto px-12 xl:px-10 2xl:px-32 !mb-12">
+    <button className="mobile-nav left absolute left-2 bottom-0 top-0" onClick={handlePrev}><FaCircleChevronLeft size={30} color={'#404041'}/></button>
+      <button  className="mobile-nav right absolute right-2 bottom-0 top-0"  onClick={handleNext}><FaCircleChevronRight size={30} color={'#404041'}/></button>
         <div className={`item bg-white z-10 flex flex-col relative text-center [box-shadow:_0_4px_4px_rgb(0_0_0_/_25%)] p-4 xl:p-10 rounded-lg rounded-t-none w-full lg:w-[32%] 2xl:w-[30%] ${activeIndex === 0 ? 'active' : ''}`} onClick={() => goToSlide(0)}>
           <div className="h-[120px] -mb-[50px]">
             <Image src={activeIndex === 0 ? '/media/images/plant-active.gif' : '/media/images/plant-static.png'} width={80} height={80} alt="Soil" className="round-image block m-auto -mt-[120px] p-[10px] w-[150px] bg-white rounded-full [box-shadow:_0_4px_4px_rgb(0_0_0_/_25%)]"/>
             </div>
             <h3 className="text-xl xl:text-3xl uppercase font-bold mb-2">Hydrocarbon</h3>
             <h4 className="text-lg xl:text-2xl text-[#5E5E5E] font-bold mb-4">ORP-SR1 & ORP-SR2</h4>
-            <p className="text-md xl:text-xl flex-auto">Hydrocarbon contamination in soil is a very serious and expensive problem throughout the world. ORP Soil Treatment 1 was launched after years of R&D to target, break and transform this type of pollution into a harmless byproduct. A revolution in the industry, fully organic and simple to inject. The results speak for themselves, SR1 & SR2 have helped reshape the reality of soil remediation.</p>
-            <Link href="/soil" className="item-button block w-4/5 m-auto border-solid border-2 color-[#404041] rounded-full text-lg xl:text-2xl px-10 py-2 border-[#404041] mt-6 font-bold">Learn more...</Link>
+            <p className="text-sm sm:text-base text-left sm:text-center xl:text-xl flex-auto">Hydrocarbon contamination in soil is a very serious and expensive problem throughout the world. ORP Soil Treatment 1 was launched after years of R&D to target, break and transform this type of pollution into a harmless byproduct. A revolution in the industry, fully organic and simple to inject. The results speak for themselves, SR1 & SR2 have helped reshape the reality of soil remediation.</p>
+            <Link href="/soil" className="item-button  block w-4/5 m-auto border-solid border-2 color-[#404041] rounded-full text-sm sm:text-lg xl:text-2xl px-10 py-2 border-[#404041] mt-6 font-bold">Learn more...</Link>
         </div>
         <div className={`item bg-white z-10 flex flex-col relative text-center [box-shadow:_0_4px_4px_rgb(0_0_0_/_25%)]  p-4 xl:p-10 rounded-lg rounded-t-none w-full lg:w-[32%] 2xl:w-[30%]  ${activeIndex === 1 ? 'active' : ''}`} onClick={() => goToSlide(1)}>
         <div className="h-[120px] -mb-[50px]">
@@ -80,8 +97,8 @@ export default function HomepageSwiper() {
             </div>
           <h3 className="text-xl xl:text-3xl uppercase font-bold mb-2">Mold</h3>
           <h4 className="text-lg xl:text-2xl text-[#5E5E5E] font-bold mb-4">ORP-MT1 & ORP-MT2</h4>
-          <p className="text-md xl:text-xl  flex-auto">Mold due to moisture events is extremely hazardous to people and employees. Mold Treatment 1&2 are ORP’s flagship product category that target and eliminate surface mold on contact. Developed as an alternative to harmful chemical options, it can be applied throughout structures and buildings simply and safely without added health risk.</p>
-          <Link href="/mold" className="item-button block w-4/5 m-auto border-solid border-2 color-[#404041] rounded-full text-lg xl:text-2xl px-10 py-2 border-[#404041] mt-6 font-bold">Learn more...</Link>
+          <p className="text-sm sm:text-base text-left sm:text-center xl:text-xl  flex-auto">Mold due to moisture events is extremely hazardous to people and employees. Mold Treatment 1&2 are ORP’s flagship product category that target and eliminate surface mold on contact. Developed as an alternative to harmful chemical options, it can be applied throughout structures and buildings simply and safely without added health risk.</p>
+          <Link href="/mold" className="item-button block w-4/5 m-auto border-solid border-2 color-[#404041] rounded-full  text-sm sm:text-lg xl:text-2xl px-10 py-2 border-[#404041] mt-6 font-bold">Learn more...</Link>
           </div>
         <div className={`item bg-white z-10 flex flex-col relative text-center [box-shadow:_0_4px_4px_rgb(0_0_0_/_25%)]  p-4 xl:p-10 rounded-lg rounded-t-none w-full lg:w-[32%] 2xl:w-[30%]  ${activeIndex === 2 ? 'active' : ''}`} onClick={() => goToSlide(2)}>
         <div className="h-[120px] -mb-[50px]">
@@ -89,8 +106,8 @@ export default function HomepageSwiper() {
             </div>
           <h3 className="text-xl xl:text-3xl uppercase font-bold mb-2">Sewage</h3>
           <h4 className="text-lg xl:text-2xl text-[#5E5E5E] font-bold mb-4">ORP-WS1 & ORP-WS2</h4>
-          <p className="text-md xl:text-xl  flex-auto">Currently in development with very promising early results, ORP-WS1 addresses the growing concern of toxic waste water and sewage. Urban sprawl and population growth have put heavy demand on governments to treat and manage sewage. WS1 is a progressive formula that can be modified to match contamination types and help solve this contamination problem.</p>
-          <Link href="/sewage" className="item-button block w-4/5 m-auto border-solid border-2 color-[#404041] rounded-full text-lg xl:text-2xl px-10 py-2 border-[#404041] mt-6 font-bold">Learn more...</Link>
+          <p className="text-sm sm:text-base text-left sm:text-center xl:text-xl  flex-auto">Currently in development with very promising early results, ORP-WS1 addresses the growing concern of toxic waste water and sewage. Urban sprawl and population growth have put heavy demand on governments to treat and manage sewage. WS1 is a progressive formula that can be modified to match contamination types and help solve this contamination problem.</p>
+          <Link href="/sewage" className="item-button block w-4/5 m-auto border-solid border-2 color-[#404041] rounded-full text-sm sm:text-lg xl:text-2xl px-10 py-2 border-[#404041] mt-6 font-bold">Learn more...</Link>
           </div>
     </div>
     </>
--- src/app/layout/ctablock.tsx
+++ src/app/layout/ctablock.tsx
@@ -9,7 +9,7 @@ export default function CTABlock() {
                 <div className="w-full flex-col lg:flex-row flex  max-w-[2000px] m-auto px-4 lg:px-32">
                     <div className="w-full lg:w-3/5 text-center lg:text-left mb-4 lg:mb-0">
                         <h3 className="text-2xl xl:text-4xl font-bold uppercase mb-2">Call to action message title goes here</h3>
-                        <p className="text-lg xl:text-2xl">Call to action message subtitle or body goes here</p>
+                        <p className="text-sm sm:text-lg xl:text-2xl">Call to action message subtitle or body goes here</p>
                     </div>
                     <div className="w-full lg:w-2/5 flex place-content-center self-center">
                         <Link href="/" className="inline-block rounded-full border-[#404041] border-solid border-4 p-2 px-12 w-auto text-lg xl:text-2xl font-bold text-[#404041] hover:bg-[#404041] hover:text-white">Enquire now</Link>
